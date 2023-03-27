cb90385028a28d8b9341db62e0ed821a18a1af72
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-27 15:29:12 -0400

    working on validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+    validates :attack, :defense, :poke_type, :image_url, null: false 
+    validates :captured, inclusion: [true, false]
+    validates :name, null: false, uniqueness: { message: "name: '%{value}' is already taken" }, length: { in: 3..255 }
+    validates :number, null: false, uniqueness: { message: ""}
+
+
 end

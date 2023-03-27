102a8c23f752ecd8d966b8b7338199f14afec6fe
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-27 16:58:28 -0400

    Starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :pokemon, only: [:index, :create, :update] 
+
+    resources :pokemon, only: [:show] do 
+      resources :items, only: [:index]
+    end
+
+  
+
+
+
+  end
 end
